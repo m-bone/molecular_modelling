8f92f73f276303fd61a75ad5856d684abdd76df6
MAB <matthewbone97@gmail.com>
2025-06-22 13:58:36 +0100

    Modified GDB testing to handle errors
    Modifed the mol descriptor generation to either dump "difficult" molecule structures or use the original input SMILES instead of the ouput  InChI

--- load_gdb.py
+++ load_gdb.py
@@ -24,29 +24,52 @@ def get_gdb_properties(file_dir: str, file_name: str, gdb_header:list) -> dict:
     
     return prop
 
-def get_smiles_inchi(file_dir: str, file_name: str) -> tuple[str, str]:
+def get_smiles_inchi(file_dir: str, file_name: str) -> tuple[list, list]:
     """
     Find and return the SMILES and InChI strings for each molecule.
     """
     with open(file_dir + "/" + file_name, "r") as f:
         lines = f.readlines()
-        smiles = lines[-2].split()[0]
-        inchi = "InChI=" + lines[-1].split("InChI=")[-1][:-1]
+        smiles = lines[-2].split()
+        inchi = lines[-1].split()
         
     return smiles, inchi
 
-def build_gdb_df(gdb_files: list[str]) -> tuple[pd.DataFrame, pd.DataFrame]:
+def build_gdb_df(gdb_dir: str, gdb_files: list[str], keep_files) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     Build two dataframes of GDB properties and molecular descriptors from RDKit.
     Save dataframes to local PostgreSQL database.
+
+    Two choices for handling difficult files:
+    Either convert all to SMILES 1 - this is GDB17 file which dictates the start of QM calc
+    Dump all difficult files for fear that the calculated QM values are garbage
     """
     gdb_values = []
     descriptions = []
     smiles_list = []
     inchi_list = []
+    error_file = []
+    persist_error = []
     for gdb_file in gdb_files:
         smiles, inchi = get_smiles_inchi(gdb_dir, gdb_file)
-        molecule = Chem.MolFromInchi(inchi)
+
+        # Select the first SMILES and second InChI
+        smiles = smiles[0]
+        inchi = inchi[1]
+
+        try:
+            molecule = Chem.MolFromInchi(inchi, sanitize=False, treatWarningAsError=True)
+            Chem.SanitizeMol(molecule)
+        except (Chem.AtomValenceException, Chem.InchiReadWriteError) as e:
+            error_file.append(gdb_file)
+            if keep_files:
+                molecule = Chem.MolFromSmiles(smiles)
+            else:
+                continue
+
+        if molecule is None:
+            persist_error.append(gdb_file)
+    
         desc = pd.DataFrame([Descriptors.CalcMolDescriptors(molecule)])
         gdb_values.append(get_gdb_properties(gdb_dir, gdb_file, GDB_HEADER))
         descriptions.append(desc)
@@ -63,17 +86,22 @@ def build_gdb_df(gdb_files: list[str]) -> tuple[pd.DataFrame, pd.DataFrame]:
     desc_df = pd.concat(descriptions, ignore_index=True)
     desc_df.insert(0, "gdb_tag", prop_df["gdb_tag"])
 
+    print(f"Persist error files:\n")
+    for name in persist_error:
+        print(f"{name}\n")
+
     return prop_df, desc_df
 
 
 if __name__ == "__main__":
-    # Note this ignores errors in molecule structures from RDKit - small number of molecules may not be parsed correctly
     gdb_dir = "/home/mab-desk/Documents/atom_dbs/gdb9"
     gdb_files = natsort.natsorted(os.listdir(gdb_dir))
     gdb_idx = (0, 133885)
+    keep_files = False
 
-    prop_df, desc_df = build_gdb_df(gdb_files[gdb_idx[0]:gdb_idx[1]])
+    prop_df, desc_df = build_gdb_df(gdb_dir, gdb_files[gdb_idx[0]:gdb_idx[1]], keep_files)
     engine = create_connection()
-    prop_df.to_sql('gdb9_properties', engine, if_exists='replace', index=False)
-    desc_df.to_sql('gdb9_descriptors', engine, if_exists='replace', index=False)
+    print(f"Length of DB: {len(desc_df)}")
+    prop_df.to_sql('gdb9_properties_cut', engine, if_exists='replace', index=False)
+    desc_df.to_sql('gdb9_descriptors_cut', engine, if_exists='replace', index=False)
     print(f"Saved GDB properties and descriptors to PostgreSQL database.")
